e8dbd194bfd6bc362cf6d9bbd48b05a5138b4735
Abbie Vance <89212624+abbievance@users.noreply.github.com>
2023-10-17 17:58:31 -0500

    add trading card

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./*.{html, js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'main' : ['Signika Negative', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
