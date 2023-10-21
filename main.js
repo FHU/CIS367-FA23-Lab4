ffee2a0f979412954badf8dad616b832eccbf6ad
gavboler03 <gavin.boler@students.fhu.edu>
2023-10-20 23:10:28 -0500

    In progress JS

--- main.js
+++ main.js
@@ -1,39 +1,8 @@
 const API_URL = "https://fhu-faculty-api.netlify.app/fhu-faculty.json"
 //const API_URL="/fhu-faculty-api.json"
 
-let cardInnerHTML = 
-`
-          <!-- BEGIN Casey -->
-          <div class="header-basic-title">Associate Professor</div>
-          <div class="header-profile-name">Kenan Casey</div>
-          <div class="header-hp">83 HP</div>
-          <div class="header-icon">☻</div>
-          <div class="profile-image"><img src="images/headshots/Caseyfam.jpg " alt="Kenan Casey"></div>
-          <div class="description-below-image-empty"><!-- empty cell to position description correctly --></div>
-          <div class="description-below-image"><span class="description-below-image-background">Math, Engineering, and Computer Science. Height: 5'8", Ph.D. in Computer Science.</span></div>
-          <div class="description-below-image-empty"><!-- empty cell to position description correctly --></div>
-          <div class="skill-cost">☻ ☻</div>
-          <div class="skill-description skill-center"><span class="skill-name">Indiscriminate Dad Joke</span></div>
-          <div class="skill-damage">62</div>
-          <div class="skill-cost">☻ ☻ ☻</div>
-          <div class="skill-description"><span class="skill-name">Ignorant Slang</span> Flip a coin. If tails, Kenan does 10 damage to himself.</div>
-          <div class="skill-damage">74</div>
-          <div class="weakness wrr-header">weakness</div>
-          <div class="resistance wrr-header">resistance</div>
-          <div class="retreat wrr-header">retreat cost</div>
-          <div class="weakness wrr-value">☻</div>
-          <div class="resistance wrr-value"></div>
-          <div class="retreat wrr-value">☻ ☻</div>
-          <div class="description-above-footer">
-            <ul>
-              <li class="description-above-footer-border">Kenan Casey, known for the hashtag #codehard. Loves children but dislikes grading homework, Comic Sans, and Chapel. LV. 6 &nbsp; #25</li>
-            </ul>
-          </div>
-          <div class="copyright"><strong>Tucker Brown</strong> ©2023, CIS367. <strong>01/61 ●</strong></div>
-        `
 
 addAllCards();
-
 async function  addAllCards() {
 
     // load the data
@@ -47,51 +16,81 @@ async function  addAllCards() {
     
     // for each item in data set (each person)
         // add that person's card to the DOM
-
+    
     people.forEach( person => {
-
-        let div = document.createElement('div');
+        let div = document.getElementById('cards');
         let cardInnerHTML = 
         `
-                  <!-- BEGIN Casey -->
-                  <div class="header-basic-title">Associate Professor</div>
-                  <div class="header-profile-name">${person.FirstName} ${person.LastName}</div>
-                  <div class="header-hp">${person.HP} HP</div>
-                  <div class="header-icon">☻</div>
-                  <div class="profile-image"><img src="images/headshots/Caseyfam.jpg " alt="Kenan Casey"></div>
-                  <div class="description-below-image-empty"><!-- empty cell to position description correctly --></div>
-                  <div class="description-below-image"><span class="description-below-image-background">Math, Engineering, and Computer Science. Height: 5'8", Ph.D. in Computer Science.</span></div>
-                  <div class="description-below-image-empty"><!-- empty cell to position description correctly --></div>
-                  <div class="skill-cost">☻ ☻</div>
-                  <div class="skill-description skill-center"><span class="skill-name">Indiscriminate Dad Joke</span></div>
-                  <div class="skill-damage">62</div>
-                  <div class="skill-cost">☻ ☻ ☻</div>
-                  <div class="skill-description"><span class="skill-name">Ignorant Slang</span> Flip a coin. If tails, Kenan does 10 damage to himself.</div>
-                  <div class="skill-damage">74</div>
-                  <div class="weakness wrr-header">weakness</div>
-                  <div class="resistance wrr-header">resistance</div>
-                  <div class="retreat wrr-header">retreat cost</div>
-                  <div class="weakness wrr-value">☻</div>
-                  <div class="resistance wrr-value"></div>
-                  <div class="retreat wrr-value">☻ ☻</div>
-                  <div class="description-above-footer">
-                    <ul>
-                      <li class="description-above-footer-border">Kenan Casey, known for the hashtag #codehard. Loves children but dislikes grading homework, Comic Sans, and Chapel. LV. 6 &nbsp; #25</li>
-                    </ul>
-                  </div>
-                  <div class="copyright"><strong>Tucker Brown</strong> ©2023, CIS367. <strong>01/61 ●</strong></div>
-                `
-
+        <div id="card" class="flex flex-col justify-center items-center text-center bg-blue-300 m-3 rounded-xl">
+            <div class="head flex flex-row flex-wrap justify-evenly bg-blue-900 mb-5 p-2 gap-8 w-full rounded-xl">
+                <div class="flex flex-col">
+                    <h1>${person.Rank}</h1>
+                    <h1>${person.FirstName} ${person.LastName}</h1>
+                </div>
+                <h1 class="mt-3">${person.Type}</h1>
+                <div class="flex flex-col">
+                    <h1>HP ${person.HitPoints}</h1>
+                    <h1>${person.EducationLevel} ${person.FieldofStudy}</h1>
+                </div>
+            </div>
+            
+            <div class="photo flex justify-center align-middle">
+                <img class="w-72" src="images/headshots/casey_Kenan.jpg" alt="">
+            </div>
+            <div class="stats flex flex-row flex-wrap justify-evenly w-72 bg-blue-900 ml-5 mr-5 rounded-b-lg p-3">
+                <h1>#${person.id}</h1>
+                <h1>${person.NickName}</h1>
+                <h1>${person.Height}</h1>
+            </div>
+            
+            <div class="cost-stamina flex flex-row flex-wrap justify-evenly w-72 m-3 bg-blue-900 p-3 ml-5 mr-5 rounded-lg">
+                <div class="flex flex-col">
+                    <h1>Cost</h1>
+                    <h1>Stamina</h1>
+                </div>
+                <div class="flex flex-col">
+                    <h1>${person.Cost}</h1>
+                    <h1>${person.Stamina}</h1>
+                </div>
+            </div>
+            <div class="attacks flex flex-row flex-wrap justify-evenly bg-blue-900 p-3 w-72 rounded-lg">
+                <div class="flex flex-col">
+                    <h1>${person.Attack1}</h1>
+                    <h1>${person.Attack2}</h1>
+                </div>
+                <div class="flex flex-col">
+                    <h1>${person.Attack1Damage}</h1>
+                    <h1>${person.Attack2Damage}</h1>
+                </div>
+            </div>
+            <div class="resistances-weaknesses flex flex-row flex-wrap justify-evenly bg-blue-900 p-3 w-72 m-3 rounded-lg">
+                <div class="flex flex-col">
+                    <h1>Resistances</h1>
+                    <h1>${person.Resistances}</h1>
+                </div>
+                <div class="flex flex-col">
+                    <h1>Weaknesses</h1>
+                    <h1>${person.Weaknesses}</h1>
+                </div>
+            </div>
+            <div class="footer flex flex-row flex-wrap justify-evenly w-full bg-black  rounded-xl p-3 ">
+                <h1>&copy;2023</h1>
+                <h1>${person.HashTag}</h1>
+                <h1>${person.Creator}</h1>
+            </div>
+        </div>
+        `
+        ;
+        
         div.innerHTML = cardInnerHTML;
 
-        document.body.appendChild(div);
+        div.append(cardInnerHTML);
+        console.log(div);
 
     });
-        
     
-
 }
 
 function addCard(person) {
 
-}
+};
