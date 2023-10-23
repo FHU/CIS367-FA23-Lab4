25f8b2bd39f7a738e2d2c283f9c13eb380096a4a
Priscilla Harris <89226780+PHarris2002@users.noreply.github.com>
2023-10-23 14:25:14 -0500

    Format Fix - 10/23 2:25pm

--- main.js
+++ main.js
@@ -1,26 +1,29 @@
-// const API_URL = "https://fhu-faculty-api.netlify.app/fhu-faculty.json"
-const API_URL = "/FHU-faculty-api.json"
+const API_URL = "https://fhu-faculty-api.netlify.app/fhu-faculty.json"
+const API_URL2 = "/FHU-faculty-api.json"
+
+const container = document.querySelector(".container")
 
 addAllCards();
 
 async function addAllCards() {
-    // load the data
+    // load the data (from netlify)
     let response = await fetch(API_URL);
     let people = await response.json();
 
     people.forEach(person => {
         let div = document.createElement('div');
+
         
         let cardInnerHTML =
-        `<section class="card-border w-[31.25rem] m-auto mt-2 p-2 bg-gradient-to-b from-gray-400 to-gray-700 font-spacegrotesk text-white rounded-2xl">
-        <section class="full-container flex flex-col ${person.Gradient} rounded-2xl">
+        `<section class="card-border w-[31.25rem] h-full m-auto mt-2 p-2 bg-gradient-to-b from-gray-400 to-gray-700 font-spacegrotesk text-white rounded-2xl">
+        <section class="full-container h-full flex flex-col bg-gradient-to-b from-indigo-950 to-slate-900 rounded-2xl">
             <section class="upper-field-section pb-4 pr-4 font-pixelify">
                 <h2 id="teaching-level" class="w-48 p-1 mb-2 bg-gradient-to-b from-gray-400 to-slate-600 rounded-br-lg text-lg">
                     ${person.Rank}
                 </h2>
                 <div class="info-subsection flex justify-between items-end mx-4">
                     <div class="flex items-end">
-                        <h1 id="name" class="w-40 text-5xl">${person.FirstName} ${person.LastName}</h1>
+                        <h1 id="name" class="w-40 text-4xl">${person.FirstName} ${person.LastName}</h1>
                         <h1 id="fantasy-class" class="text-2xl text-emerald-200">${person.Type}</h1>
                     </div>
         
@@ -33,8 +36,8 @@ async function addAllCards() {
         
             <section class="image-section flex justify-center">
                 <div class="character-image relative h-80 w-[26.4rem] flex justify-center bg-gradient-to-b border-2">
-                    <img src="images/headshots/pixelized_Casey.png" class="object-cover w-full">
-                    <p class="absolute flex left-0 bg-gradient-to-b from-gray-400 to-slate-600 w-32 h-10 justify-center items-center font-pixelify text-3xl">${person.EducationLevel}</p>
+                    <img src="https://fhu-faculty-api.netlify.app/images/headshots/${person.Image}" alt="${person.FirstName} ${person.LastName}" class="object-cover w-full">
+                    <p class="absolute flex left-0 bg-gradient-to-b from-gray-400 to-slate-600 w-32 h-10 justify-center items-center font-pixelify text-2xl">${person.EducationLevel}</p>
                     <img>
                 </div>
             </section>
@@ -86,6 +89,7 @@ async function addAllCards() {
         
 
         document.body.appendChild(div);
+        container.append(div);
     });
 
 }
