d84c537b3a890f4da63b9cb61ba548edb89a9a61
Abbie Vance <89212624+abbievance@users.noreply.github.com>
2023-10-23 16:10:17 -0500

    Fix card height
    made all the cards the same height and changed some design issues

--- main.js
+++ main.js
@@ -57,7 +57,7 @@ async function addAllCards(){
     `      
 
     <div
-      class="card border-2 relative shadow-2xl bg-gradient-to-br from-neutral-900 ${backgroundColors[person.FieldofStudy]} mt-5"
+      class="card border-2 relative shadow-2xl bg-gradient-to-br from-neutral-900 ${backgroundColors[person.FieldofStudy]} h-full"
     >
       <div class="square w-5 h-5 absolute top-0 left-0"></div>
       <div class="square w-5 h-5 absolute top-0 right-0"></div>
@@ -67,24 +67,23 @@ async function addAllCards(){
       <div class="line w-full h-1 absolute bottom-4 left-0"></div>
       <div class="line h-full w-1 absolute left-4 top-0"></div>
       <div class="line h-full w-1 absolute right-4 top-0"></div>
+      <div class="line h-full w-1 absolute left-4 top-0"></div>
 
       <div
-        class="triangle-container relative p-6 m-5 bg-gradient-to-br from-neutral-900 ${backgroundColors[person.FieldofStudy]}"
+        class="triangle-container relative m-5 bg-gradient-to-br from-neutral-900 ${backgroundColors[person.FieldofStudy]} m-2"
       >
         <div class="triangle-right-top"></div>
-        <div class="triangle-right-bottom"></div>
         <div class="triangle-left-top"></div>
         <div class="triangle-left-bottom"></div>
-        <div class="image flex flex-col items-center mt-2">
-          <div class="line w-full h-1 absolute top-7 left-0"></div>
-          <div class="line w-full h-1 absolute bottom-7 left-0"></div>
+        <div class="triangle-right-bottom"></div>
+        <div class="image flex flex-col items-center mt-2 border-b-4 w-full">
           <img
-            class="w-2/3"
+            class="w-2/3 border-l-4 border-r-4"
             src="https://fhu-faculty-api.netlify.app/images/headshots/${person.Image}"
             alt="${person.FirstName} ${person.LastName}"
           />
         </div>
-        <div class="name-container flex flex-col items-center">
+        <div class="name-container flex flex-col items-center ml-2">
           <h2 class="font-main text-md text-gray-300">
           ${person.NickName} | ${person.Type} | ${person.Rank} | ${person.EducationLevel}
           </h2>
@@ -93,12 +92,12 @@ async function addAllCards(){
           </h1>
         </div>
 
-        <div class="attack-container mt-2 relative">
+        <div class="attack-container mt-2 relative ml-2">
           <div class="flex items-center justify-between">
             <h1 class="font-main text-lg text-gray-200 font-bold">
             ${person.Attack1}
             </h1>
-            <h1 class="font-main text-gray-200 text-md">
+            <h1 class="font-main text-gray-200 text-md mr-2">
             ${person.Attack1Damage}
             </h1>
           </div>
@@ -106,14 +105,14 @@ async function addAllCards(){
             <h1 class="font-main text-lg text-gray-200 font-bold">
             ${person.Attack2}
             </h1>
-            <h1 class="font-main text-gray-200 text-md">
+            <h1 class="font-main text-gray-200 text-md mr-2">
             ${person.Attack2Damage}
             </h1>
           </div>
         </div>
 
 
-        <div class="weakness-container grid grid-cols-2 mt-2 relative">
+        <div class="weakness-container grid grid-cols-2 mt-2 relative h-20 ml-2">
           <div class="col-start-1">
             <h1 class="font-main text-lg text-gray-200 font-bold">
               Resistances
@@ -129,11 +128,26 @@ async function addAllCards(){
           </div>
         </div>
 
+        <div class="stats-numbers-container flex justify-between items-center p-2 border-b-4 min-h-full">
+        <h1 class="font-main text-md text-gray-200 font-bold">
+        <id class="text-lg"> ${person.HitPoints}</id>  <id class="text-gray-300 text-sm">HP</id>
+        </h1>
+        <h1 class="font-main text-md text-gray-200 font-bold">
+        <id class="text-lg"> ${person.DamageType}</id>   <id class="text-gray-300 text-sm">Damage</id>
+        </h1>
+        <h1 class="font-main text-md text-gray-200 font-bold">
+        <id class="text-lg"> ${person.Stamina}</id>   <id class="text-gray-300 text-sm">Stamina</id>
+        </h1>
+        </div>
 
 
-        <h1 class="font-main text-gray-400 mb-1 text-right text-sm mt-2">
+
+        <h1 class="font-main text-gray-400 text-right text-sm mr-12 py-1">
         ${person.HashTag}
         </h1>
+
+        <div class="line w-full h-1 absolute bottom-0 left-0"></div>
+        
       </div>`
 
         // document.querySelector(".container").innerHTML = cardInnterHTML;
