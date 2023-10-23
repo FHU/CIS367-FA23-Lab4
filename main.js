7f97e1e67caa2e4a545f5845537b5a8f05f2ae86
kayleeodom <kaylee.odom@students.fhu.edu>
2023-10-23 16:30:54 -0500

    working

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
-//const API_URL = "https://fhu-faculty-api.netlify.app/fhu-faculty.json"
+const API_URL = "https://fhu-faculty-api.netlify.app/fhu-faculty.json"
 
-const API_URL = "fhu-faculty-api.json"
-
-addAllCards();
+//const API_URL = "fhu-faculty-api.json"
 
 async function addAllCards() {
     
@@ -27,20 +25,13 @@ async function addAllCards() {
                 <div class="flex flex-row justify-around text-white border-b-2 border-solid p-2">
                     <div class="flex flex-col">
                         <p class="text-sm border-2 border-solid rounded-lg text-center">${person.Rank}</p>
-                        <p class="font-bold text-lg">${person.FirstName} '${person.NickName}' ${person.LastName}</p>
-                    </div>
-                            
-                    <div class="flex flex-col text-right">
-                        <div class="flex flex-row text-left">
-                            <p class="px-2 font-bold">HP${person.HitPoints}</p>
-                            <span class="material-symbols-outlined">stars</span>
-                        </div>
-                        <p class="text-right">${person.FieldofStudy}</p>
+                        <p class="font-bold">${person.FirstName} '${person.NickName}' ${person.LastName}</p>
+                        <p class="">${person.FieldofStudy}</p>
                     </div>
                 </div>
                         
                 <div class="flex flex-col items-center m-2">
-                    <img class="object-scale-down border-2 border-solid rounded-t-lg" src="images/headshots/pierce_sarah.jpg">
+                    <img class="w-2/3" src="https://fhu-faculty-api.netlify.app/images/headshots/${person.Image}" alt="${person.FirstName} ${person.LastName}"/>
                     <p class="text-center text-white w-full text-sm border-b-2 border-r-2 border-l-2 
                         border-solid rounded-b-lg p-1 pb-2">NO.439 ${person.FirstName} ${person.LastName} HT:${person.Height}</p>
                 </div>
@@ -48,7 +39,7 @@ async function addAllCards() {
                 <div class="text m-2">
                     <div class="flex flex-row justify-start text-white">
                         <span class="material-symbols-outlined text-xl pt-1 pl-3">stars</span>
-                        <p class="font-bold text-center text-xl pl-10">${person.Attack1}</p>
+                        <p class="font-bold text-center text-lg pl-10">${person.Attack1}</p>
                     </div>
                     <p class="py-3 px-2 text-white text-sm">Prevent all damage done to this Pokemon by attacks from Basic Pokemon during 
                         your apponenets next turn. This pokemon can't use Quick Guard during your next turn.</p>
@@ -70,19 +61,19 @@ async function addAllCards() {
                 <div class="flex flex-row m-2 justify-around pt-3 border-t-2 border-solid">
                     <div class="flex flex-row text-xs border-r-2 border-solid">
                         <span class="material-symbols-outlined text-red-400">local_fire_department</span>
-                        <p class="text-red-400 font-semibold">Weakness : ${person.Weaknesses}</p>
+                        <p class="text-red-400 font-semibold">Weakness: ${person.Weaknesses}</p>
                     </div>
                                     
                     <div class="flex flex-row text-xs pl-4">
                         <span class="pb-4 material-symbols-outlined text-purple-400">mystery</span>
-                        <p class="text-purple-400 font-semibold">Resistance : ${person.Resistances}</p>
+                        <p class="text-purple-400 font-semibold">Resistance: ${person.Resistances}</p>
                     </div>
                                     
                 </div>
                                     
                 <div class="flex flex-row justify-evenly text-white p-2">
                     <p class="w-1/3">@2023</p>
-                    <p class="w-1/3">CodeHard</p>
+                    <p class="w-1/3">#CodeHard</p>
                     <p class="w-1/3">Kaylee Odom</p>
                 </div>
                             
@@ -91,10 +82,12 @@ async function addAllCards() {
         div.innerHTML = cardInnerHTML;
 
         document.body.appendChild(div);
+
+
     });
 }
                             
-                            
+addAllCards();                         
 function addCard(person){
 
 }
