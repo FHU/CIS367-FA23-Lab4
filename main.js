d99751ed7ab47826007f2d80f619b40ceee55960
kayleeodom <kaylee.odom@students.fhu.edu>
2023-11-04 23:54:23 -0500

    work

--- main.js
+++ main.js
@@ -2,19 +2,6 @@ const API_URL = "https://fhu-faculty-api.netlify.app/fhu-faculty.json"
 
 //const API_URL = "fhu-faculty-api.json"
 
-function drawCardOnCanvas(person){
-    const canvas = document.createElement("canvas");
-    const ctx = canvas.getContext("2d")
-
-    canvas.width = 800;
-    canvas.height = 800;
-
-    ctx.fillStyle = "white"
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-    return canvas;
-}
-
 async function addAllCards() {
     
     // load the data
@@ -32,79 +19,81 @@ async function addAllCards() {
         let div = document.createElement('div');
         
         let cardInnerHTML = 
-        `<div class="card">
-            
-            <div class="top">
-                <div class="basic">
-                    <p class="Rank">${person.Rank}</p>
-                    <p class="Hp">HP${person.HitPoints}</p>
-                </div>
-                <div class="description">
-                    <p class="title">${person.FirstName} "${person.NickName}" ${person.LastName}</p>
-                    <p class="field">${person.FieldofStudy}</p>
-                </div>
-            </div>
-            
-            <div class="picture-holder">
-            <img class="profile" src="https://fhu-faculty-api.netlify.app/images/headshots/${person.Image} " alt="${person.FirstName} ${person.LastName}">
-                <p class="summary">NO.439 ${person.FirstName} ${person.LastName} HT:${person.Height}"</p>
-            </div>
-                    
-            <div class="holder">
-                <div class="skills">
-                    <span class="material-symbols-outlined">stars</span>
-                    <p class="attack">${person.Attack1}</p>
-                    <p class="points">${person.Attack1Damage}+</p>
-                </div>
-                <p class="sentence">Prevent all damage done to this Pokemon by attacks from Basic Pokemon during 
-                    your apponenets next turn. This pokemon can't use Quick Guard during your next turn.</p>
-                            
-                <div class="skills">
-                    <div class="stars">
-                        <span class="material-symbols-outlined">stars</span>
-                        <span class="material-symbols-outlined">stars</span>
+        `<div class="whole">
+            <div class="card">
+                
+                <div class="top">
+                    <div class="basic">
+                        <p class="Rank">${person.Rank}</p>
+                        <p class="Hp">HP${person.HitPoints}</p>
                     </div>
-                    <p class="attack">${person.Attack2}</p>
-                    <p class="points">${person.Attack2Damage}+</p>
+                    <div class="description">
+                        <p class="title">${person.FirstName} ${person.LastName}</p>
+                        <p class="field">${person.FieldofStudy}</p>
+                    </div>
                 </div>
                 
-                <p class="sentence">This attack does 30 more damage for each Prize Card 
-                    your opponent has taken.
-                </p>
-            </div>
-                                
-            <div class="section">
-                <div class="weakness-section ">
-                    <span class="material-symbols-outlined">local_fire_department</span>
-                    <p class="weakness">Weakness: ${person.Weaknesses}</p>
+                <div class="picture-holder">
+                <img class="profile" src="https://fhu-faculty-api.netlify.app/images/headshots/${person.Image} " alt="${person.FirstName} ${person.LastName}">
+                    <p class="summary">NO.439 ${person.FirstName} ${person.LastName} HT:${person.Height}"</p>
                 </div>
-                                
-                <div class="resistance-section">
-                    <span class="pb-2 material-symbols-outlined">mystery</span>
-                    <p class="resistance">Resistance: ${person.Resistances}</p>
-                </div>
-                                
-            </div>
-                                
-            <div class="bottom">
-                <p class="year">@2023</p>
-                <p class="hash">#CodeHard</p>
-                <p class="designer">Kaylee Odom</p>
-            </div>
                         
-        </div>
-        
-        <div class="buttons">
-            <button class="like-btn" id="like">
-                <i class="material-icons liked-icon">favorite</i>
-                <i class="material-icons unliked-icon">favorite_border</i>
-            </button>
-            <button class="download-btn" id="download">
-                <i class="material-symbols-outlined">download</i>
-            </button>
-            <button class="share-btn" id="share">
-                <i class="material-symbols-outlined">share</i>
-            </button>
+                <div class="holder">
+                    <div class="skills">
+                        <span class="material-symbols-outlined">stars</span>
+                        <p class="attack">${person.Attack1}</p>
+                        <p class="points">${person.Attack1Damage}+</p>
+                    </div>
+                    <p class="sentence">Prevent all damage done to this Pokemon by attacks from Basic Pokemon during 
+                        your apponenets next turn. This pokemon can't use Quick Guard during your next turn.</p>
+                                
+                    <div class="skills">
+                        <div class="stars">
+                            <span class="material-symbols-outlined">stars</span>
+                            <span class="material-symbols-outlined">stars</span>
+                        </div>
+                        <p class="attack">${person.Attack2}</p>
+                        <p class="points">${person.Attack2Damage}+</p>
+                    </div>
+                    
+                    <p class="sentence">This attack does 30 more damage for each Prize Card 
+                        your opponent has taken.
+                    </p>
+                </div>
+                                    
+                <div class="section">
+                    <div class="weakness-section ">
+                        <span class="material-symbols-outlined">local_fire_department</span>
+                        <p class="weakness">Weakness: ${person.Weaknesses}</p>
+                    </div>
+                                    
+                    <div class="resistance-section">
+                        <span class="pb-2 material-symbols-outlined">mystery</span>
+                        <p class="resistance">Resistance: ${person.Resistances}</p>
+                    </div>
+                                    
+                </div>
+                                    
+                <div class="bottom">
+                    <p class="year">@2023</p>
+                    <p class="hash">#CodeHard</p>
+                    <p class="designer">Kaylee Odom</p>
+                </div>
+                            
+            </div>
+            
+            <div class="buttons">
+                <button class="like-btn" id="like">
+                    <i class="material-icons liked-icon">favorite</i>
+                    <i class="material-icons unliked-icon">favorite_border</i>
+                </button>
+                <button class="download-btn" id="download">
+                    <i class="material-symbols-outlined">download</i>
+                </button>
+                <button class="share-btn" id="share">
+                    <i class="material-symbols-outlined">share</i>
+                </button>
+            </div>
         </div>`;
 
         div.innerHTML = cardInnerHTML;
@@ -119,6 +108,8 @@ async function addAllCards() {
 
         // Download controls
         const downloadbtn = div.querySelector('.download-btn');
+        
+        // Share controls
         const sharebtn = div.querySelector('.share-btn');
 
 
@@ -137,26 +128,7 @@ async function addAllCards() {
 
         // Download a card as PNG
         function downloadCard(){
-            const canvas = drawCardOnCanvas(person)
-            const dataURL = canvas.toDataURL("image/png");
             
-            // Need to add with what content I want drawn on the canvas
-            const cardHTML = div.querySelector('.card').outerHTML;
-
-            const img = new Image();
-            img.src = 'data:image/svg+mxl,' + encodeURIComponent(cardHTML);
-
-            img.onload = () => {
-                ctx.drawImage(img, 0, 0);
-
-                const dataURL = canvas.toDataURL("image/png");
-
-                const downloadLink = document.createElement("a");
-                downloadLink.href = dataURL;
-                downloadLink.download = "downloaded_image.png";
-
-                downloadLink.click();
-            };
         }
 
         // Share a card through email
@@ -177,6 +149,7 @@ async function addAllCards() {
         sharebtn.addEventListener("click", shareCard);
     });
 }
+
                             
 addAllCards();                         
 function addCard(person){
